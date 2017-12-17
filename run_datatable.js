e2c506ed1f50412de162fc41ab06b8b6dd062462
edgonhn <34605425+edgonhn@users.noreply.github.com>
2017-12-17 15:19:03 -0600

    Update run_datatable.js

--- run_datatable.js
+++ run_datatable.js
@@ -1,7 +1,7 @@
 var csv = null;
 var filter_csv = null;
 var my_data_table = null;
-d3.text("https://www.dropbox.com/s/98reav1wtbcch0i/HN%20TABLE%20INDICATORS.csv?raw=1", function(text) {
+d3.text("https://edgonhn.github.io/CAREHN2020/HN TABLE INDICATORS.csv", function(text) {
   csv = d3.csv.parseRows(text);
   $(document).ready(function() {
     $('#demo').html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="example"></table>' );
@@ -11,8 +11,8 @@ d3.text("https://www.dropbox.com/s/98reav1wtbcch0i/HN%20TABLE%20INDICATORS.csv?r
               { "title": "CI_Global_Impact" },
               { "title": "CI_Indicator" },
               { "title": "HN_Project" },
-			  { "title": "HN_Indicator" },
-	          { "title": "Reach_ppl", "class": "center" },
+	      { "title": "HN_Indicator" },
+	      { "title": "Reach_ppl", "class": "center" },
           ]
       } );   
 } );
